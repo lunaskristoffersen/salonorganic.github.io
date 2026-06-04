08a899d3660555d584f5a79abc081b55d10e8c62
lunaskristoffersen <lunaskristoffersen@gmail.com>
2026-06-04 17:05:27 +0200

    jsupdate

--- script.js
+++ script.js
@@ -16,13 +16,3 @@ document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click", (
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
-
-
-$(document).ready(function () {
-  function reorient(e) {
-    var portrait = (window.orientation % 180 == 0);
-    $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
-  }
-  window.onorientationchange = reorient;
-  window.setTimeout(reorient, 0);
-});
